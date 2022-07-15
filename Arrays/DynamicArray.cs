55128f7f87104b94abec4d61db47c3fb98f31ecf
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-07-15 15:18:17 +0300

    my solution to exercises

--- Arrays/DynamicArray.cs
+++ Arrays/DynamicArray.cs
@@ -47,6 +47,78 @@ public class DynamicArray
         return -1;
     }
 
+    public int[] ToArray()
+    {
+        int[] result = new int[this.count];
+
+        for (int i = 0; i < this.count; i++)
+            result[i] = this.items[i];
+
+        return result;
+    }
+
+    public static DynamicArray FromArray(int[] array)
+    {
+        DynamicArray result = new (array.Length);
+        for (int i = 0; i < array.Length; i++)
+            result.Insert(array[i]);
+
+        return result;
+    }
+
+    public int Max() {
+        // O(n)
+        int max = 0;
+
+        for (int i = 0; i < this.count; i++)
+            if(this.items[i] > max)
+                max = this.items[i];
+
+        return max;
+    }
+
+    public DynamicArray Intersect(DynamicArray other)
+    {
+        // O(n ^ 2)
+        int[] otherArray = other.ToArray();
+        DynamicArray result = new (otherArray.Length);
+        for (int i = 0; i < this.count; i++)
+            for (int j = 0; j < otherArray.Length; j++)
+                if(this.items[i] == otherArray[j])
+                    result.Insert(this.items[i]);
+
+        return result;
+    }
+
+    public void Reverse() 
+    {
+        // O(n)
+        DynamicArray reversed = new(this.count);
+
+        for (int i = this.count - 1; i >= 0; i--)
+            reversed.Insert(this.items[i]);
+
+        this.items = reversed.ToArray();
+    }
+
+    public void InsertAt(int item, int index)
+    {
+        // O(n)
+        if(index < 0 || index > this.count - 1)
+            throw new IndexOutOfRangeException();
+
+        int itemNext = this.items[index];
+
+        for (int i = index; i < this.count - 1; i++)
+        {
+            this.items[i] = item;
+            item = this.items[i + 1];
+            this.items[i + 1] = itemNext;
+        }
+
+        this.Insert(item);
+    }
+
     public void Print()
     {
         for (int i = 0; i < this.count; i++)
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -7,5 +7,31 @@ numbers.Insert(20);
 numbers.Insert(30);
 numbers.Insert(40);
 numbers.RemoveAt(index: 3);
+
 numbers.Print();
+
+WriteLine("numbers.InsertAt(15, 1):");
+numbers.InsertAt(15, 1);
+numbers.Print();
+
+WriteLine("numbers.InsertAt(100, 3):");
+numbers.InsertAt(100, 3);
+numbers.Print();
+
+WriteLine("numbers.InsertAt(1000, 0):");
+numbers.InsertAt(1000, 0);
+numbers.Print();
+
+numbers.Reverse();
+
+WriteLine("Reverse:");
+numbers.Print();
+
 WriteLine(numbers.IndexOf(40));
+
+WriteLine($"Max: {numbers.Max()}");
+
+DynamicArray intersected = numbers.Intersect(DynamicArray.FromArray(new int[] { 10, 25, 20 }));
+WriteLine($"Intersect with [10, 15, 20]:");
+intersected.Print();
+
