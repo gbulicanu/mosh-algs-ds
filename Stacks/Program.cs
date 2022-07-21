28a56d9fa8df6f9fc786b7e2f90eaff2ae86587e
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-07-21 10:10:51 +0300

    add mosh string reverser

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -1,6 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
 using static System.Console;
 
-Stack<char> stack = new("abcd");
+string input = "abcd";
+Stack<char> stack = new(input);
+StringReverser reverser = new();
+var revereseOutput = reverser.Reverse(input);
 
-Write(new string(stack.ToArray()));
+WriteLine(new string(stack.ToArray()));
+WriteLine(revereseOutput);
+
+class StringReverser
+{
+    internal string Reverse(string input)
+    {
+        ArgumentNullException.ThrowIfNull(input);
+        Stack<char> charStack = new Stack<char>();
+        foreach (var character in input)
+        {
+            charStack.Push(character);
+        }
+
+        StringBuilder result = new();
+        while(charStack.Count != 0)
+        {
+            result.Append(charStack.Pop());
+        }
+
+        return result.ToString();
+    }
+}
