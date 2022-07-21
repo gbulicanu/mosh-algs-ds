f3ccb7e49e52dac0d566b0bfcc6a3b552311b519
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-07-21 12:44:31 +0300

    small refactor

--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -1,32 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Text;
 using static System.Console;
+
+var p = (string value) => WriteLine(value);
+var pb = (char[]? buffer) => WriteLine(buffer);
 
 string input = "abcd";
 Stack<char> stack = new(input);
-StringReverser reverser = new();
-var revereseOutput = reverser.Reverse(input);
+string revereseOutput = StringReverser.Reverse(input);
 
-WriteLine(new string(stack.ToArray()));
-WriteLine(revereseOutput);
-
-class StringReverser
-{
-    internal string Reverse(string input)
-    {
-        ArgumentNullException.ThrowIfNull(input);
-        Stack<char> charStack = new Stack<char>();
-        foreach (var character in input)
-        {
-            charStack.Push(character);
-        }
-
-        StringBuilder result = new();
-        while(charStack.Count != 0)
-        {
-            result.Append(charStack.Pop());
-        }
-
-        return result.ToString();
-    }
-}
+pb(stack.ToArray());
+p(revereseOutput);
---
+++ Stacks/StringReverser.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+class StringReverser
+{
+    internal static string Reverse(string input)
+    {
+        ArgumentNullException.ThrowIfNull(input);
+        Stack<char> charStack = new Stack<char>();
+        foreach (var character in input)
+        {
+            charStack.Push(character);
+        }
+
+        StringBuilder result = new();
+        while(charStack.Count != 0)
+        {
+            result.Append(charStack.Pop());
+        }
+
+        return result.ToString();
+    }
+}
