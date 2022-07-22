165ea41d41c19c934597ae24f7d5a5529f78ba2e
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-07-22 14:46:06 +0300

    my solution to dynamic stack

---
+++ Stacks/DynamicStack.cs
@@ -0,0 +1,65 @@
+﻿class DynamicStack
+{
+    private int[] items;
+
+    private int count;
+
+    public DynamicStack(int intialCapacity = 3)
+    {
+        this.items = new int[intialCapacity];
+    }
+
+    public void Push(int item)
+    {
+        if (items.Length == this.count)
+        {
+            int[] newItems = new int[items.Length * 2];
+
+            for (int i = 0; i < this.count; i++)
+                newItems[i] = this.items[i];
+
+            this.items = newItems;
+        }
+
+        this.items[this.count++] = item;
+    }
+    
+    public int Pop()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack is empty");
+
+        return this.items[--this.count];
+    }
+    
+    public int Peek()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack is empty");
+
+        return this.items[this.count - 1];
+    }
+    
+    public bool IsEmpty()
+    {
+        return this.count == 0;
+    }
+
+    public int[] ToArray()
+    {
+        int[] result = new int[this.count];
+        for (int i = this.count - 1; i >= 0; i--)
+            result[i] = this.items[i];
+
+        return result;
+    }
+
+    public override string ToString()
+    {
+        string[] itemsRep = this.ToArray()
+            .Select(i => i.ToString())
+            .ToArray();
+
+        return $"[{string.Join(" -> ", itemsRep)}>";
+    }
+}
--- Stacks/Program.cs
+++ Stacks/Program.cs
@@ -8,7 +8,21 @@ using static System.Console;
 //WriteLine(stack.ToArray());
 //WriteLine(revereseOutput);
 
-string input = "[1 + 1]";
+//string input = "[1 + 1]";
 
-bool isBalanced = BalancedExpressions.IsBalanced(input);
-WriteLine($"isBalanced('{input}'): {isBalanced}");
+//bool isBalanced = BalancedExpressions.IsBalanced(input);
+//WriteLine($"isBalanced('{input}'): {isBalanced}");
+
+DynamicStack stack = new();
+
+WriteLine(stack.IsEmpty());
+//stack.Peek();
+//stack.Pop();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+WriteLine($"Peek: {stack.Peek()}");
+stack.Push(4);
+stack.Push(5);
+WriteLine($"Pop: {stack.Pop()}");
+WriteLine(stack);
