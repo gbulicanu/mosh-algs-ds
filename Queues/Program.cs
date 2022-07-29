490021c123f48fcbc39205add0186202fca3099e
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-07-29 10:30:55 +0300

    my solution to reverse queue

---
+++ Queues/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+static void Reverse(Queue<int> input)
+{
+    Stack<int> stack = new (input);
+    input.Clear();
+    while (stack.TryPop(out var value))
+    {
+        input.Enqueue(value);
+    }
+}
+
+static void PrintQueue(Queue<int> input)
+{
+    var array = input.ToArray()
+        .Select(x => x.ToString())
+        .ToArray();
+    WriteLine($"[{string.Join(", ", array)}]");
+}
+
+Queue<int> input = new();
+input.Enqueue(10);
+input.Enqueue(20);
+input.Enqueue(30);
+
+Reverse(input);
+
+PrintQueue(input);
+
