40a0b88bd9ed0aa1459ccc4cab30732b93746f83
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-07-29 11:31:44 +0300

    my implementation to array queue

---
+++ Queues/ArrayQueue.cs
@@ -0,0 +1,69 @@
+﻿class ArrayQueue
+{
+    readonly int capacity;
+
+    int[] items;
+    int frontIndex;
+    int rearIndex;
+
+    public ArrayQueue(int capacity = 10)
+    {
+        this.capacity = capacity;
+        this.items = new int[capacity];
+    }
+
+    public bool IsFull()
+    {
+        return this.rearIndex == this.capacity;
+    }
+
+    public bool IsEmpty()
+    {
+        return this.rearIndex == 0 && this.frontIndex == 0;
+    }
+
+    public void Enqueue(int item)
+    {
+        if (this.IsFull())
+            throw new InvalidOperationException("Queue is full");
+
+        this.items[this.rearIndex++] = item;
+    }
+
+    public int Dequeue()
+    {
+        if (this.IsEmpty())
+            throw new InvalidOperationException("Queue is empty");
+
+        var result = this.items[this.frontIndex++];
+        if(this.frontIndex == this.rearIndex)
+            this.frontIndex = this.rearIndex = 0;
+
+        return result;
+    }
+
+    public int Peek()
+    {
+        if (this.IsEmpty())
+            throw new InvalidOperationException("Queue is empty");
+
+        return this.items[this.frontIndex];
+    }
+
+    public override string ToString()
+    {
+        if (IsEmpty())
+            return "[]";
+
+        var queueArray = new int[this.Count];
+
+        Array.Copy(this.items, this.frontIndex, queueArray, 0, this.Count);
+        var queueArrayRep = queueArray
+            .Select(x => x.ToString())
+            .ToArray();
+
+        return $"[{string.Join(", ", queueArrayRep)}]";
+    }
+
+    public int Count { get { return this.rearIndex - this.frontIndex; } }
+}
--- Queues/Program.cs
+++ Queues/Program.cs
@@ -17,6 +17,7 @@ static void PrintQueue(Queue<int> input)
     WriteLine($"[{string.Join(", ", array)}]");
 }
 
+// Reverse queue
 Queue<int> input = new();
 input.Enqueue(10);
 input.Enqueue(20);
@@ -26,3 +27,41 @@ Reverse(input);
 
 PrintQueue(input);
 
+// ArrayQueue implementation
+ArrayQueue arrayQueue = new ();
+WriteLine(arrayQueue);
+WriteLine($"IsFull: {arrayQueue.IsFull()}");
+WriteLine($"IsEmpty: {arrayQueue.IsEmpty()}");
+WriteLine("Enqueue(10)");
+arrayQueue.Enqueue(10);
+WriteLine(arrayQueue);
+WriteLine($"IsFull: {arrayQueue.IsFull()}");
+WriteLine($"IsEmpty: {arrayQueue.IsEmpty()}");
+arrayQueue.Enqueue(20);
+arrayQueue.Enqueue(30);
+WriteLine(arrayQueue);
+
+WriteLine($"Peek: {arrayQueue.Peek()}");
+WriteLine(arrayQueue);
+
+WriteLine($"Dequeue: {arrayQueue.Dequeue()}");
+WriteLine(arrayQueue);
+WriteLine($"Dequeue: {arrayQueue.Dequeue()}");
+WriteLine(arrayQueue);
+WriteLine($"Dequeue: {arrayQueue.Dequeue()}");
+WriteLine(arrayQueue);
+//WriteLine("Attempt dequeue empty:")
+//WriteLine(arrayQueue);
+arrayQueue.Enqueue(10);
+arrayQueue.Enqueue(20);
+arrayQueue.Enqueue(30);
+arrayQueue.Enqueue(40);
+arrayQueue.Enqueue(50);
+arrayQueue.Enqueue(60);
+arrayQueue.Enqueue(70);
+arrayQueue.Enqueue(80);
+arrayQueue.Enqueue(90);
+arrayQueue.Enqueue(100);
+// WriteLine("Attempt enqueue on full:")
+// arrayQueue.Enqueue(110);
+WriteLine(arrayQueue);
