881549b55687d24beefda3c8059efd2ec52daf25
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-07-30 13:14:57 +0300

    my solution to first repeated character

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -4,10 +4,15 @@
 // ex, 'a green apple' => 'g'
 
 string input = "a green apple";
+string inputRepeated = "green apple";
 
 int notRepeatedIndex = FindFirstNotRepeatingIn(input);
 WriteLine($"First no repeating character in '{input}' is" +
     $" '{input[notRepeatedIndex]}' found at index {notRepeatedIndex}.");
+
+int repeatedIndex = FindFirstRepeatingIn(inputRepeated);
+WriteLine($"First repeating character in '{inputRepeated}' is" +
+    $" '{inputRepeated[repeatedIndex]}' found at index {repeatedIndex}.");
 
 int FindFirstNotRepeatingIn(string input)
 {
@@ -25,4 +30,20 @@ int FindFirstNotRepeatingIn(string input)
 			return i;
 
 	return -1;
+}
+
+int FindFirstRepeatingIn(string input)
+{
+    HashSet<char> charsSet = new();
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        char c = input[i];
+        int currentCount = charsSet.Count;
+        charsSet.Add(c);
+        if (currentCount == charsSet.Count)
+            return i;
+    }
+
+    return -1;
 }
