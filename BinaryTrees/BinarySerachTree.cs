8787220eec5ece64e69fcee9231020689d7cdf96
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-08-01 12:02:56 +0300

    my solution to binary search tree

---
+++ BinaryTrees/BinarySerachTree.cs
@@ -0,0 +1,68 @@
+﻿class BinarySerachTree
+{
+    Node? rootNode;
+
+    public void Insert(int value)
+    {
+        if (rootNode == null)
+        {
+            rootNode = new Node(value);
+            return;
+        }
+
+        Node? currentNode = rootNode;
+        while (currentNode != null)
+        {
+            Node currentParent = currentNode;
+            if (value < currentNode.Value)
+            {
+                currentNode = currentNode.Left;
+                if (currentNode == null)
+                    currentParent.Left = new Node(value, parent: currentParent);
+            }
+            else
+            {
+                currentNode = currentNode.Right;
+                if (currentNode == null)
+                    currentParent.Right = new Node(value, parent: currentParent);
+            }
+        }
+    }
+
+    public bool Find(int value)
+    {
+        if (rootNode == null)
+            return false;
+
+
+        Node? currentNode = rootNode;
+        while (currentNode != null)
+        {
+            if (currentNode.Value == value)
+                return true;
+
+            if (value < currentNode.Value)
+                currentNode = currentNode.Left;
+            else
+                currentNode = currentNode.Right;
+        }
+
+        return false;
+    }
+
+    private class Node
+    {
+        public int Value;
+        public Node? Left;
+        public Node? Right;
+        public Node? Parent;
+
+        public Node(int value, Node? left = null, Node? right = null, Node? parent = null)
+        {
+            this.Value = value;
+            this.Left = left;
+            this.Right = right;
+            this.Parent = parent;
+        }
+    }
+}
---
+++ BinaryTrees/Program.cs
@@ -0,0 +1,30 @@
+﻿WriteLine("");
+WriteLine("Binary Trees");
+WriteLine("============");
+
+BinarySerachTree bst = new();
+
+WriteLine("Insert(10)");
+bst.Insert(10);
+WriteLine("Insert(5)");
+bst.Insert(5);
+WriteLine("Insert(15)");
+bst.Insert(15);
+WriteLine("Insert(6)");
+bst.Insert(6);
+WriteLine("Insert(1)");
+bst.Insert(1);
+WriteLine("Insert(8)");
+bst.Insert(8);
+WriteLine("Insert(12)");
+bst.Insert(12);
+WriteLine("Insert(18)");
+bst.Insert(18);
+WriteLine("Insert(17)");
+bst.Insert(17);
+
+WriteLine($"Find(1): {bst.Find(1)}");
+WriteLine($"Find(2): {bst.Find(2)}");
+WriteLine($"Find(3): {bst.Find(3)}");
+WriteLine($"Find(17): {bst.Find(17)}");
+
