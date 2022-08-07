676bb2c1d0a4551ed018bc67e4691a4dae3b8b50
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-08-07 11:50:46 +0300

    check edge cases and refactor

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -2,8 +2,11 @@
 WriteLine("Bubble Sort");
 WriteLine("===========");
 
-static void BubbleSort(int[] array)
+static void BubbleSort(int[]? array)
 {
+	if (array == null)
+		return;
+
 	int passesRequired = array.Length / 2 + 1;
 
 	int pass = 0;
@@ -18,7 +21,19 @@ static void BubbleSort(int[] array)
 	}
 }
 
-int[] array = { 8, 2, 4, 1, 3 };
-BubbleSort(array);
-WriteLine($"array(after sorting):[{string.Join(", ", array)}]");
+int[] array1 = { 8, 2, 4, 1, 3 };
+int[] array2 = { 8, 2, 4, 1 };
+int[] array3 = { 8, 4, 3, 1 };
+int[] array4 = Array.Empty<int>();
+int[]? array5 = null;
+
+BubbleSort(array1);
+BubbleSort(array2);
+BubbleSort(array3);
+BubbleSort(array4);
+BubbleSort(array5);
+WriteLine($"array1(after sorting):[{string.Join(", ", array1)}]");
+WriteLine($"array2(after sorting):[{string.Join(", ", array2)}]");
+WriteLine($"array3(after sorting):[{string.Join(", ", array3)}]");
+WriteLine($"array4(after sorting):[{string.Join(", ", array4)}]");
 
