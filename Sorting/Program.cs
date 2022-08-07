5da685e8c0a3b3e0d1291214d1a664dde2bae76d
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-08-07 14:16:58 +0300

    my solution to insertion sort

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -50,6 +50,27 @@ static void SelectionSort(int[]? array)
     }
 }
 
+static void InsertionSort(int[]? array)
+{
+    if (array == null)
+        return;
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        int current = array[i];
+        int insertIndex = 0;
+
+        for (int j = i - 1; j >= 0; j--)
+            if (current < array[j])
+            {
+                array[j + 1] = array[j];
+                insertIndex = j;
+            }
+        
+        array[insertIndex] = current;
+    }
+}
+
 int[] array1bs = { 8, 2, 4, 1, 3 };
 int[] array2bs = { 8, 2, 4, 1 };
 int[] array3bs = { 8, 4, 3, 1 };
@@ -89,4 +110,24 @@ WriteLine($"array1ss(after sorting):[{string.Join(", ", array1ss)}]");
 WriteLine($"array2ss(after sorting):[{string.Join(", ", array2ss)}]");
 WriteLine($"array3ss(after sorting):[{string.Join(", ", array3ss)}]");
 WriteLine($"array4ss(after sorting):[{string.Join(", ", array4ss)}]");
+
+int[] array1is = { 8, 2, 4, 1, 3 };
+int[] array2is = { 8, 2, 4, 1 };
+int[] array3is = { 8, 4, 3, 1 };
+int[] array4is = Array.Empty<int>();
+int[]? array5is = null;
+
+WriteLine("");
+WriteLine("Insertion Sort");
+WriteLine("==============");
+
+InsertionSort(array1is);
+InsertionSort(array2is);
+InsertionSort(array3is);
+InsertionSort(array4is);
+InsertionSort(array5is);
+WriteLine($"array1is(after sorting):[{string.Join(", ", array1is)}]");
+WriteLine($"array2is(after sorting):[{string.Join(", ", array2is)}]");
+WriteLine($"array3is(after sorting):[{string.Join(", ", array3is)}]");
+WriteLine($"array4is(after sorting):[{string.Join(", ", array4is)}]");
 
