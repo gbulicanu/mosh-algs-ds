2632be2e7dac4f3216d5c0e81a447be2151a5b9f
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-08-07 12:59:30 +0300

    refactor usage

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -50,37 +50,43 @@ static void SelectionSort(int[]? array)
     }
 }
 
-int[] array1 = { 8, 2, 4, 1, 3 };
-int[] array2 = { 8, 2, 4, 1 };
-int[] array3 = { 8, 4, 3, 1 };
-int[] array4 = Array.Empty<int>();
-int[]? array5 = null;
+int[] array1bs = { 8, 2, 4, 1, 3 };
+int[] array2bs = { 8, 2, 4, 1 };
+int[] array3bs = { 8, 4, 3, 1 };
+int[] array4bs = Array.Empty<int>();
+int[]? array5bs = null;
 
-//WriteLine("");
-//WriteLine("Bubble Sort");
-//WriteLine("===========");
+WriteLine("");
+WriteLine("Bubble Sort");
+WriteLine("===========");
 
-//BubbleSort(array1);
-//BubbleSort(array2);
-//BubbleSort(array3);
-//BubbleSort(array4);
-//BubbleSort(array5);
-//WriteLine($"array1(after sorting):[{string.Join(", ", array1)}]");
-//WriteLine($"array2(after sorting):[{string.Join(", ", array2)}]");
-//WriteLine($"array3(after sorting):[{string.Join(", ", array3)}]");
-//WriteLine($"array4(after sorting):[{string.Join(", ", array4)}]");
+BubbleSort(array1bs);
+BubbleSort(array2bs);
+BubbleSort(array3bs);
+BubbleSort(array4bs);
+BubbleSort(array5bs);
+WriteLine($"array1bs(after sorting):[{string.Join(", ", array1bs)}]");
+WriteLine($"array2bs(after sorting):[{string.Join(", ", array2bs)}]");
+WriteLine($"array3bs(after sorting):[{string.Join(", ", array3bs)}]");
+WriteLine($"array4bs(after sorting):[{string.Join(", ", array4bs)}]");
+
+int[] array1ss = { 8, 2, 4, 1, 3 };
+int[] array2ss = { 8, 2, 4, 1 };
+int[] array3ss = { 8, 4, 3, 1 };
+int[] array4ss = Array.Empty<int>();
+int[]? array5ss = null;
 
 WriteLine("");
 WriteLine("Selection Sort");
 WriteLine("==============");
 
-SelectionSort(array1);
-SelectionSort(array2);
-SelectionSort(array3);
-SelectionSort(array4);
-SelectionSort(array5);
-WriteLine($"array1(after sorting):[{string.Join(", ", array1)}]");
-WriteLine($"array2(after sorting):[{string.Join(", ", array2)}]");
-WriteLine($"array3(after sorting):[{string.Join(", ", array3)}]");
-WriteLine($"array4(after sorting):[{string.Join(", ", array4)}]");
+SelectionSort(array1ss);
+SelectionSort(array2ss);
+SelectionSort(array3ss);
+SelectionSort(array4ss);
+SelectionSort(array5ss);
+WriteLine($"array1ss(after sorting):[{string.Join(", ", array1ss)}]");
+WriteLine($"array2ss(after sorting):[{string.Join(", ", array2ss)}]");
+WriteLine($"array3ss(after sorting):[{string.Join(", ", array3ss)}]");
+WriteLine($"array4ss(after sorting):[{string.Join(", ", array4ss)}]");
 
