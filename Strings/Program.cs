238ec2a64c5c9974ae00b98971a75a07bb062776
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-08-10 23:42:24 +0300

    solution to count vowels

---
+++ Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿WriteLine();
+WriteLine("Count Vowels");
+WriteLine("============");
+
+string input = "hello";
+
+static int CountVowels(string input)
+{
+    if (input == null)
+        return 0;
+
+    string vowels = "aeiou";
+    int count = 0;
+    foreach (char c in input.ToLower())
+        if (vowels.IndexOf(c) != -1)
+            count++;
+
+    return count;
+
+}
+
+WriteLine($"CountVowels(\"{input}\"): {CountVowels(input)}");
