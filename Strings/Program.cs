7895d5fce27148dd1023be3c9b81774e45b0342a
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2022-08-10 23:43:23 +0300

    small refactor

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -2,14 +2,14 @@
 WriteLine("Count Vowels");
 WriteLine("============");
 
-string input = "hello";
+string? input = "hello";
 
-static int CountVowels(string input)
+static int CountVowels(string? input)
 {
     if (input == null)
         return 0;
 
-    string vowels = "aeiou";
+    const string vowels = "aeiou";
     int count = 0;
     foreach (char c in input.ToLower())
         if (vowels.IndexOf(c) != -1)
